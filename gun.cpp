166d41be366ff195f622ca683c48b17c2fa2f840
Meghan <SanguineBird@users.noreply.github.com>
2015-04-01 04:53:27 -0400

    Create gun.cpp
    untested

---
+++ gun.cpp
@@ -0,0 +1,23 @@
+/*This is the implementation file for the class 'Gun'.*/
+
+#include <string>
+using std::string;
+#include "gun.h"
+
+namespace gunsMMoore{
+  Gun::Gun() : manufacturer("Unknown Manufacturer"), caliber("Unknown Caliber")
+  {/*deliberately empty*/}
+  
+  Gun::Gun(const string& theManufacturer, const string& theCaliber) : manufacturer(theManufacturer), caliber(theCaliber)
+  {/*deliberately empty*/}
+  
+  string Gun::getManufacturer() const {return manufacturer;}
+  
+  string Gun::getCaliber() const {return caliber;}
+  
+  void Gun::setManufacturer(const string& theManufacturer)
+  {manufacturer = theManufacturer}
+  
+  void Gun::setCaliber(const string& theCaliber)
+  {caliber = theCaliber}
+}
