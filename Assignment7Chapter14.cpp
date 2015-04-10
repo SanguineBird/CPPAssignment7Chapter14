b9a1dd556d92caf2b426985a4740898bdeec8d1d
Meghan <SanguineBird@users.noreply.github.com>
2015-04-10 05:24:21 -0400

    Update Assignment7Chapter14.cpp
    Final Code

--- Assignment7Chapter14.cpp
+++ Assignment7Chapter14.cpp
@@ -2,27 +2,35 @@
 Meghan Moore
 March 28, 2015
 CIT 245 - Data Structures and Programming C++
-Assignment 7 - Chapter 14
-Purpose: Demonstrate inheritance by utilizing the classes 'Revolver' and 'Pistol'.
-These classes are derived from 'Handgun', which is derived from 'Gun'.
+Assignment 7 - Chapter 13
+Purpose: Implement the Towers of Hanoi using recursion.
 */
 
 #include <iostream> //cin, cout, cerr, endl
 #include <cstdlib> //system
 using namespace std;
-#include "revolver.h"
-#include "pistol.h"
+#include "TowersOfHanoi.h"
+#include "RepeatPrompt.h"
+using namespace RepeatMMoore;
 
 int main(){
-  Revolver rev;
-  Pistol pis;
-  
-  rev.Revolver("Smith & Wesson", "0.357", "hoague grips", "Trijicon", true, false, 6)
-  pis.Pistol("Glock", "9mm", "manufacturer's grips", "3 dot sights", false, true)
-  
-  cout << "Revolver: " << rev.getManufacturer() << ", " << rev.getCaliber() << ", " << rev.getGrips() << ", " << rev.getSights() << ", " << rev.printLaser() << ", " << rev.printSingleAction() << ", " << rev.printNumberOfRounds() << endl;
-  
-  cout << "Pistol: " << pis.getManufacturer() >> ", " << pis.getCaliber() << ", " << pis.getGrips() << ", " << pis.getSights() << ", " << pis.printLaser() << ", " << pis.printSemiauto() << endl;
+  int totalDiscs;
+  RepeatPrompt repeat;
+
+  do{
+    {using namespace TowersMMoore;
+      
+      cout << "Enter number of disks:\n";
+      cin >> totalDiscs;
+      
+      TowersOfHanoi towers(totalDiscs); //constructor
+      cout << "All discs begin on tower A.\nThe destination is tower B.\nThe spare tower is C.\n\n";
+      
+      towers.moveDisc(towers.getMaxIndex(), 'A', 'B', 'C');
+      cout << "Number of moves: " << towers.getMoves() << endl << endl;
+      
+    } //end namespace TowersMMoore
+  }while (repeat.repeatYN());
   
   system("pause");
   return 0;
